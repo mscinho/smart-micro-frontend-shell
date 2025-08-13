3a1e0230d6fc00df9fffc1f1cf4fc55cc569f9ca
Márcio <marcio.soares@essentia.com.br>
2025-08-12 23:46:16 -0300

    Aplicado design na pagina de registro e funcionalidade.

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value !== confirmPassword.value ? { passwordMismatch: true } : null;
+};
 
 @Component({
   selector: 'app-register',
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
 })
 export class Register implements OnInit {
   registerForm!: FormGroup;
+  errorMessage: string | null = null;
+  hidePassword = true;
 
   constructor(
     private fb: FormBuilder,
@@ -35,18 +48,18 @@ export class Register implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required]]
-    });
+    }, { validators: passwordMatchValidator });
   }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
+      this.errorMessage = null;
       this.authService.register(this.registerForm.value).subscribe({
         next: () => {
           this.router.navigate(['/auth/login']);
         },
         error: (error) => {
-          console.error('Registration failed:', error);
-          // TODO: Exibir uma mensagem de erro para o usuário
+          this.errorMessage = error.error.message || 'Ocorreu um erro no servidor.';
         }
       });
     }
